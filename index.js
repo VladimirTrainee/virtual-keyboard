d7d61025818564ee49800058aa80f79b216b7786
Vladimir Kirpichyov <vlasoul@mail.ru>
2023-05-01 10:48:14 +0300

    feat: add keyboard.js

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-import {DomNode} from './domnode.js';
+import {KeyBoard} from './keyboard.js';
+
+const splitDigit = (val) => { return {code: `Digit${val[0]}`, value: val[0], shiftValue: val[1] }};
+const splitLetter = (val) => { return {code: `Key${String(val).charAt(0)}`, value: val, shiftValue: String(val).toLowerCase(), hideShiftValue: true }};
+
+const keyBoard = new KeyBoard([
+  { code: 'Backquote', value: '`', shiftValue: '~' },
+  ...[['1', '!'], ['2', '@'], ['3', '#'], ['4', '$'], ['5', '%'], ['6', '^'], ['7', '&'], ['8', '*'], ['9', '('], ['0', ')']].map(splitDigit),
+  { code: 'Minus', value: '-', shiftValue: '_' },
+  { code: 'Equal', value: '=', shiftValue: '+' },
+  { code: 'Backspace', value: 'Backspace', width: '2.8', endLine: true, inverse: true },
+  { code: 'Tab', value: 'Tab', width: '1.4', inverse: true },
+  ...['QЙ', 'WЦ', 'EУ', 'RК', 'TЕ', 'YН', 'UГ', 'IШ', 'OЩ', 'PЗ'].map(splitLetter),
+  { code: 'BracketLeft', value: '[Х', shiftValue: '{'} ,
+  { code: 'BracketRight', value: ']Ъ', shiftValue: '}' },
+  { code: 'Backslash', value: '\\', shiftValue: '|' },
+  { code: 'Delete', value: 'DEL', width: '1.4', endLine: true, inverse: true },
+  { code: 'CapsLock', value: 'Caps Lock', width: '2.7', inverse: true },
+  ...['AФ', 'SЫ', 'DВ', 'FА', 'GП', 'HР', 'JО', 'KЛ', 'LД'].map(splitLetter),
+  { code: 'Semicolon', value: ';Ж', shiftValue: ':' },
+  { code: 'Quote', value: '\'Э', shiftValue: '"' },
+  { code: 'Enter', value: 'ENTER', width: '2.1', endLine: true, inverse: true },
+  { code: 'ShiftLeft', value: 'Shift', width: '2.7', inverse: true },
+  ...['ZЯ', 'XЧ', 'CС', 'VМ', 'BИ', 'NТ', 'MЬ'].map(splitLetter),
+  { code: 'Comma', value: ',Б', shiftValue: '<' },
+  { code: 'Period', value: '.Ю', shiftValue: '>' },
+  { code: 'Slash', value: '/', shiftValue: '?' },
+  { code: 'ArrowUp', value: '↑' },
+  { code: 'ShiftRight', value: 'Shift', width: '2.1', endLine: true, inverse: true },
+  { code: 'ControlLeft', value: 'Ctrl', width: '1.4', inverse: true } ,
+  { code: 'MetaLeft', value: 'Win', width: '1.4', inverse: true },
+  { code: 'AltLeft', value: 'Alt', width: '1.3', inverse: true },
+  { code: 'Space', value: ' ', width: '4.9', inverse: true },
+  { code: 'AltRight', value: 'Alt', width: '1.3', inverse: true },
+  { code: 'ControlRight', value: 'Ctrl', width: '1.4', inverse: true },
+  { code: 'ArrowLeft', value: '←' },
+  { code: 'ArrowDown', value: '↓' },
+  { code: 'ArrowRight', value: '→' },
+  { code: 'Language', width: '1.1', inverse: true }
+]);
+
+keyBoard.setParent(document.getElementById('main'))
+  .createNode();
+
---
+++ keyboard.js
@@ -0,0 +1,86 @@
+import {DomNode} from './domnode.js';
+
+export class KeyBoard {
+    constructor(keys = []) {
+      this.CapsLock = false;
+      this.ShiftLeft = false;
+      this.ShiftRight = false;
+      this.keyOrder = [];
+      this.keys = Object.fromEntries(keys.map((key, index) => { 
+        const keyDetails = { ...key, order: index };
+        this.keyOrder.push(key.code); 
+        return [key.code, keyDetails];
+      }));
+  
+      this.languages = ['En', 'Ru'];
+      this.languageIndex = 0;
+    }
+    
+    resetSequenceKeys() {
+      const keyList = [
+        this.keys.ShiftLeft.code,
+        this.keys.ShiftRight.code,
+        this.keys.ControlLeft.code,
+        this.keys.ControlRight.code,
+        this.keys.AltLeft.code,
+        this.keys.AltRight.code
+      ];
+      
+      for (const key of keyList) {
+        this[key] = false;
+        document.getElementById(`${key}-button`).classList.value = 'key-button2';
+      }
+        
+      return this;
+    } 
+    setEndBoardKey(key) {
+      this.endBoardKeys.push(key);
+      return this;
+    }
+  
+    setParent(parent) {
+      this.parent = parent;
+      this.domNode = new DomNode(this.parent);
+      return this;
+    }
+  
+    createNode() {
+      this.domNode.addNode({ tag: 'div', className: 'board' })
+        .setNewParent()
+        .addNode({ tag: 'form' })
+        .setNewParent()
+        .addNode({ tag: 'textarea', className : 'keyboard-input', id: 'keyboard-input' })
+        .setParent();
+      for (let key of this.keyOrder) {
+        const  {
+          code, 
+          value = this.languages[this.languageIndex],
+          shiftValue, 
+          width = '1', 
+          hideShiftValue = false, 
+          inverse = false 
+        } = this.keys[key];
+        const label = (inverse) ? value : String(value).charAt(this.languageIndex);
+        const keyWidth = `${+width * 6.3}%`;
+        const keyHeight = '50px';
+
+        this.domNode.addNode({ tag: 'button', className: ((inverse) ? 'key-button2' : 'key-button'), id: `${code}-button` })
+          .setStyle({ width: keyWidth, height: keyHeight })
+          .setNewParent();
+        if (shiftValue && !hideShiftValue) {
+          this.domNode.addNode({ tag: 'sup', className: 'key-small', innerText: shiftValue });
+        }
+        this.domNode.addNode({ tag: 'span', className: 'key-main', innerText: label, id: `${code}-label` })
+          .setParent();
+
+      }
+
+      return this;
+    }
+
+    nextLanguage() {
+      this.languageIndex++;
+      this.languageIndex %= this.languages.length;
+      return this;
+    }
+  }
